d0cff1e41af16a435e25703dfba139aa572b4cb6
andrii-grynik <andrii.grynik@gmail.com>
2023-03-09 19:24:15 -0700

    "Admin Dashboard update"

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
+    @product_count = Product.all.count
+    @first_count = Product.where(category_id: 1).count
+    @second_count = Product.where(category_id: 2).count
+    @third_count = Product.where(category_id: 3).count
   end
 end
