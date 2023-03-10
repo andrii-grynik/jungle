074c59b3abfa3515d8e51f37a631f5bb7981205f
andrii-grynik <andrii.grynik@gmail.com>
2023-03-09 21:13:11 -0700

    "Admin categories"

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,9 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def show
-    @product_count = Product.all.count
-    @first_count = Product.where(category_id: 1).count
-    @second_count = Product.where(category_id: 2).count
-    @third_count = Product.where(category_id: 3).count
+    @category = Category.all
+    @product = Product.all
   end
 end
