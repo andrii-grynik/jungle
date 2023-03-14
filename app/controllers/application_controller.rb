62c3bd44e9df1ab2024a221fa260b46066cac9fd
andrii-grynik <andrii.grynik@gmail.com>
2023-03-13 18:01:34 -0600

    "Cart and other minor fixes"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
+
   protect_from_forgery with: :exception
-
-  private
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -14,6 +11,8 @@ class ApplicationController < ActionController::Base
   def authorize
     redirect_to '/login' unless current_user
   end
+
+  private
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
@@ -26,10 +25,9 @@ class ApplicationController < ActionController::Base
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
-    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
+    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum / 100.00
   end
   helper_method :cart_subtotal_cents
-
 
   def update_cart(new_cart)
     cookies[:cart] = {
@@ -38,4 +36,5 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
 end
